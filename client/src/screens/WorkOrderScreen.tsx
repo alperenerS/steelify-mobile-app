30bbfa66f2f3c3e17620bde2cdb3df49c05666e8
emremataraci <mtrcemre@gmail.com>
2023-06-12 14:41:31 +0300

    requests are thrown at the same time for WOScreen

--- client/src/screens/WorkOrderScreen.tsx
+++ client/src/screens/WorkOrderScreen.tsx
@@ -1,3 +1,5 @@
+// client/src/screens/WorkOrderScreen.tsx
+
 import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, FlatList, Button, Image, SafeAreaView } from 'react-native';
 import { getWorkById, getForm, postQualityControl } from '../services/workService'; 
@@ -29,16 +31,27 @@ const WorkOrderScreen = ({route}: {route: WorkOrderScreenRouteProp}) => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await getWorkById(workId);
-      setWork(data.workInfo);
+      const workDataPromise = getWorkById(workId);
+      const productInfoPromise = getProductInfo(productId);
+      
+      const [workData, productInfoResponse] = await Promise.all([workDataPromise, productInfoPromise]);
 
-      const formResponse = await getForm(productId, data.workInfo[0].vendor_id);
-      setFormId(formResponse.form[0].id); 
+      setWork(workData.workInfo);
+      setProductInfo(productInfoResponse.productInfo[0]);
+      navigation.setOptions({ title: productInfoResponse.productInfo[0].name });
 
-      const qualityControlResponse = await postQualityControl(formResponse.form[0].id, workId); 
+      const formResponsePromise = getForm(productId, workData.workInfo[0].vendor_id);
+      const formResponse = await formResponsePromise;
+
+      setFormId(formResponse.form[0].id);
+
+      const qualityControlResponsePromise = postQualityControl(formResponse.form[0].id, workId);
+      const qualityControlResponse = await qualityControlResponsePromise;
 
       const qualityControlIds = qualityControlResponse.qualitycontrol.map(qc => qc.id);
-      const imageCountData = await getImageCounts(qualityControlIds, workId);
+      const imageCountDataPromise = getImageCounts(qualityControlIds, workId);
+      const imageCountData = await imageCountDataPromise;
+
       const imageCountRecord: Record<string, number> = {};
 
       imageCountData.forEach(ic => {
@@ -51,13 +64,8 @@ const WorkOrderScreen = ({route}: {route: WorkOrderScreenRouteProp}) => {
       }));
 
       setQualityControlData(updatedQualityControlData);
-
-      const productInfoResponse = await getProductInfo(productId);
-      setProductInfo(productInfoResponse.productInfo[0]);
-
-      navigation.setOptions({ title: productInfoResponse.productInfo[0].name });
     };
-    
+
     fetchData();
   }, [workId]);
 
@@ -67,71 +75,71 @@ const WorkOrderScreen = ({route}: {route: WorkOrderScreenRouteProp}) => {
 
   return (
     <SafeAreaView style={{ flex: 1, marginBottom: 80 }}>
-    <View>
-      {work.length > 0 ? (
-        <>
-          <Text>Work ID: {work[0].id}, Vendor ID: {work[0].vendor_id}, QR ID: {work[0].quality_responsible_id}, Form ID: {formId}</Text>
-          {productInfo ? (
-          <View style={buttonstyles.buttonContainer}>
-            <Button
-              title="Teknik Çizim"
-              onPress={() => {
-                if (productInfo?.technicaldrawingurl) {
-                  navigation.navigate('PdfViewerScreen', { pdfUrl: productInfo.technicaldrawingurl })
+      <View>
+        {work.length > 0 ? (
+          <>
+            <Text>Work ID: {work[0].id}, Vendor ID: {work[0].vendor_id}, QR ID: {work[0].quality_responsible_id}, Form ID: {formId}</Text>
+            {productInfo ? (
+            <View style={buttonstyles.buttonContainer}>
+              <Button
+                title="Teknik Çizim"
+                onPress={() => {
+                  if (productInfo?.technicaldrawingurl) {
+                    navigation.navigate('PdfViewerScreen', { pdfUrl: productInfo.technicaldrawingurl })
+                  }
+                }}
+              />
+              <Button
+                title="Kılavuz"
+                onPress={() => {
+                  if (productInfo?.guideurl) {
+                    navigation.navigate('PdfViewerScreen', { pdfUrl: productInfo.guideurl })
+                  }
+                }}
+              />
+            </View>
+          ) : (
+            <Text>Loading product info...</Text>
+          )}
+            <FlatList 
+              data={qualityControlData}
+              keyExtractor={(item, index) => `key-${index}`}
+              renderItem={({item}) => {
+                // Directly use item.imageCount
+                const imageCount = item.imageCount;
+                // If imageCount is not equal to sample_quantity, then render the item
+                if(imageCount !== item.sample_quantity) {
+                  return (
+                    <View style={workstyles.card}>
+                        <View style={{flexDirection: 'row'}}>
+                            <View style={{flex: 0.1, justifyContent: 'center', alignItems: 'center'}}>
+                                <Text style={workstyles.text}>{imageCount} / {item.sample_quantity}</Text>
+                            </View>
+                            <View style={{flex: 0.7}}>
+                                <Text style={workstyles.text}>{item.step_name}</Text>
+                                <Text style={workstyles.text}>Teknik Çizim Numarası: {item.technical_drawing_numbering}</Text>
+                                <Text style={workstyles.text}>ID: {item.id}</Text>
+                            </View>
+                            <View style={{flex: 0.2, justifyContent: 'center', alignItems: 'center'}}>
+                              <TouchableOpacity onPress={() => navigation.navigate('Kamera', { example_visual_url: item.example_visual_url || null, workId: item.work_id , quality_control_id: item.id, productId: productId })}>
+                                <Image source={cameraIcon} style={{width: 40, height: 40}} />
+                              </TouchableOpacity>
+                              <Text style={workstyles.text2}>Fotoğraf Çek</Text>
+                            </View>
+                        </View>
+                    </View>
+                  );
+                }
+                else {
+                  return null;
                 }
               }}
             />
-            <Button
-              title="Kılavuz"
-              onPress={() => {
-                if (productInfo?.guideurl) {
-                  navigation.navigate('PdfViewerScreen', { pdfUrl: productInfo.guideurl })
-                }
-              }}
-            />
-          </View>
+          </>
         ) : (
-          <Text>Loading product info...</Text>
+          <Text>Loading...</Text>
         )}
-          <FlatList 
-            data={qualityControlData}
-            keyExtractor={(item, index) => `key-${index}`}
-            renderItem={({item}) => {
-              // Directly use item.imageCount
-              const imageCount = item.imageCount;
-              // If imageCount is not equal to sample_quantity, then render the item
-              if(imageCount !== item.sample_quantity) {
-                return (
-                  <View style={workstyles.card}>
-                      <View style={{flexDirection: 'row'}}>
-                          <View style={{flex: 0.1, justifyContent: 'center', alignItems: 'center'}}>
-                              <Text style={workstyles.text}>{imageCount} / {item.sample_quantity}</Text>
-                          </View>
-                          <View style={{flex: 0.7}}>
-                              <Text style={workstyles.text}>{item.step_name}</Text>
-                              <Text style={workstyles.text}>Teknik Çizim Numarası: {item.technical_drawing_numbering}</Text>
-                              <Text style={workstyles.text}>ID: {item.id}</Text>
-                          </View>
-                          <View style={{flex: 0.2, justifyContent: 'center', alignItems: 'center'}}>
-                            <TouchableOpacity onPress={() => navigation.navigate('Kamera', { example_visual_url: item.example_visual_url || null, workId: item.work_id , quality_control_id: item.id, productId: productId })}>
-                              <Image source={cameraIcon} style={{width: 40, height: 40}} />
-                            </TouchableOpacity>
-                            <Text style={workstyles.text2}>Fotoğraf Çek</Text>
-                          </View>
-                      </View>
-                  </View>
-                );
-              }
-              else {
-                return null;
-              }
-            }}
-          />
-        </>
-      ) : (
-        <Text>Loading...</Text>
-      )}
-    </View>
+      </View>
     </SafeAreaView>
   );
 };
