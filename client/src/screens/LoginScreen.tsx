0dade95264c0d0604a5c7bfc6cde306a08bcb656
emremataraci <mtrcemre@gmail.com>
2023-06-13 11:44:22 +0300

    login details are now remembered

--- client/src/screens/LoginScreen.tsx
+++ client/src/screens/LoginScreen.tsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, TextInput, Alert, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack'; // import StackNavigationProp
+import { StackNavigationProp } from '@react-navigation/stack';
 import { login } from '../services/authService';
-import { storeData } from '../utils/storage';
+import { storeData, getData } from '../utils/storage';
 import loginstyles from '../components/Login';
 import yenalogo from '../assets/yena_logo.png';
 
 type RootStackParamList = {
-    Login: undefined;
-    Main: undefined;
-  }
-  
+  Login: undefined;
+  Main: undefined;
+};
+
 type LoginScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
   'Login'
@@ -20,35 +20,58 @@ type LoginScreenNavigationProp = StackNavigationProp<
 const LoginScreen = () => {
   const [phone, setPhone] = useState('');
   const [password, setPassword] = useState('');
-  const navigation = useNavigation<LoginScreenNavigationProp>(); // add type for useNavigation
+  const navigation = useNavigation<LoginScreenNavigationProp>();
+
+  useEffect(() => {
+    const retrieveData = async () => {
+      const storedPhone = await getData('userPhone');
+      const storedPassword = await getData('userPassword');
+      if (storedPhone) setPhone(storedPhone);
+      if (storedPassword) setPassword(storedPassword);
+    };
+    retrieveData();
+  }, []);
 
   const handleLogin = async () => {
     try {
       const token = await login(phone, password);
       await storeData('userToken', token);
-      navigation.navigate('Main'); // navigate to Main screen after login
+      await storeData('userPhone', phone);
+      await storeData('userPassword', password);
+      navigation.navigate('Main');
     } catch (error) {
       if (error instanceof Error) {
         Alert.alert('Hata', error.message);
       }
     }
   };
-
-  
   return (
     <View style={loginstyles.container}>
-      <Image source={yenalogo} style={loginstyles.logo}/>
-      <View style={loginstyles.inputView} >
-        <TextInput style={loginstyles.inputText} placeholder="Telefon Numarası" placeholderTextColor="black" onChangeText={setPhone} value={phone} />
+      <Image source={yenalogo} style={loginstyles.logo} />
+      <View style={loginstyles.inputView}>
+        <TextInput
+          style={loginstyles.inputText}
+          placeholder="Telefon Numarası"
+          placeholderTextColor="black"
+          onChangeText={setPhone}
+          value={phone}
+        />
       </View>
-      <View style={loginstyles.inputView} >
-        <TextInput style={loginstyles.inputText} placeholder="Parola" placeholderTextColor="black" onChangeText={setPassword} value={password} secureTextEntry />
-      </View>    
+      <View style={loginstyles.inputView}>
+        <TextInput
+          style={loginstyles.inputText}
+          placeholder="Parola"
+          placeholderTextColor="black"
+          onChangeText={setPassword}
+          value={password}
+          secureTextEntry
+        />
+      </View>
       <TouchableOpacity style={loginstyles.loginBtn} onPress={handleLogin}>
-          <Text style={loginstyles.loginText}>Giriş Yap</Text>
+        <Text style={loginstyles.loginText}>Giriş Yap</Text>
       </TouchableOpacity>
     </View>
-  );  
+  );
 };
 
 export default LoginScreen;
