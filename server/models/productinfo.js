5940283cf160a8332b2c109ed72645ff69291aa8
yenademir <yena@yenaengineering.nl>
2023-07-12 19:06:20 +0300

    Odooid

--- server/models/productinfo.js
+++ server/models/productinfo.js
@@ -2,7 +2,7 @@ const db = require("../config/db");
 
 const ProductInfo = {
   findByProductId: async (product_id) => {
-    return await db.any('SELECT id, name, technicaldrawingurl, guideurl FROM products WHERE id = $1', [product_id]);
+    return await db.any('SELECT id, name, technicaldrawingurl, guideurl FROM products WHERE odooid = $1', [product_id]);
   }
 };
 
