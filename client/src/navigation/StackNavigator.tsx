25c41aaa89e345e619e647ee25a57b64de138937
emremataraci <mtrcemre@gmail.com>
2023-08-02 15:08:41 +0300

    User can navigate WorkOrderScreen from PDFViewer

--- client/src/navigation/StackNavigator.tsx
+++ client/src/navigation/StackNavigator.tsx
@@ -1,21 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, {useEffect, useState} from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import TabNavigator from './TabNavigator';
 import LoginScreen from '../screens/LoginScreen';
 import WorkOrderScreen from '../screens/WorkOrderScreen';
-import PdfViewerScreen from '../screens/PdfViewerScreen'; 
-import CameraScreen from '../screens/CameraScreen'; 
-import PreviewScreen from '../screens/PreviewScreen'; 
-import { getData } from '../utils/storage';
+import PdfViewerScreen from '../screens/PdfViewerScreen';
+import CameraScreen from '../screens/CameraScreen';
+import PreviewScreen from '../screens/PreviewScreen';
+import {getData} from '../utils/storage';
 
 export type RootStackParamList = {
   Login: undefined;
   Main: undefined;
-  WorkOrderScreen: { workId: number; productId: number };
-  PdfViewerScreen: { pdfUrl: string };
-  Kamera: { example_visual_url: string | null, workId: number, quality_control_id: number , productId: number, technical_drawing_numbering: string, lower_tolerance: string, upper_tolerance: string, step_name: string, order_number: number, product_name: string | null, vendor_id: number};
-  Önizleme: { pictureUri: string, example_visual_url: string | null, workId: number, quality_control_id: number, productId: number, technical_drawing_numbering: string, lower_tolerance: string, upper_tolerance: string, step_name: string, order_number: number, product_name: string | null, vendor_id: number  };
+  WorkOrderScreen: {workId: number; productId: number};
+  PdfViewerScreen: {pdfUrl: string; workId: number; productId: number};
+  Kamera: {
+    example_visual_url: string | null;
+    workId: number;
+    quality_control_id: number;
+    productId: number;
+    technical_drawing_numbering: string;
+    lower_tolerance: string;
+    upper_tolerance: string;
+    step_name: string;
+    order_number: number;
+    product_name: string | null;
+    vendor_id: number;
+  };
+  Önizleme: {
+    pictureUri: string;
+    example_visual_url: string | null;
+    workId: number;
+    quality_control_id: number;
+    productId: number;
+    technical_drawing_numbering: string;
+    lower_tolerance: string;
+    upper_tolerance: string;
+    step_name: string;
+    order_number: number;
+    product_name: string | null;
+    vendor_id: number;
+  };
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -43,15 +68,23 @@ const StackNavigator = () => {
   }
 
   return (
-    <Stack.Navigator initialRouteName={userToken ? "Main" : "Login"}>
-      <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}}/>
-      <Stack.Screen name="Main" component={TabNavigator} options={{headerShown: false}}/>
-      <Stack.Screen name="WorkOrderScreen" component={WorkOrderScreen}/>
-      <Stack.Screen name="PdfViewerScreen" component={PdfViewerScreen}/>
-      <Stack.Screen name="Kamera" component={CameraScreen}/>
-      <Stack.Screen name="Önizleme" component={PreviewScreen}/> 
+    <Stack.Navigator initialRouteName={userToken ? 'Main' : 'Login'}>
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Main"
+        component={TabNavigator}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen name="WorkOrderScreen" component={WorkOrderScreen} />
+      <Stack.Screen name="PdfViewerScreen" component={PdfViewerScreen} />
+      <Stack.Screen name="Kamera" component={CameraScreen} />
+      <Stack.Screen name="Önizleme" component={PreviewScreen} />
     </Stack.Navigator>
   );
-}
+};
 
 export default StackNavigator;
