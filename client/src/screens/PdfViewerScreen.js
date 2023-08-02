27cbf8c5b5932ef92beeab10950d950391d00acc
emremataraci <mtrcemre@gmail.com>
2023-08-02 16:24:50 +0300

    fixed navigate & props

--- client/src/screens/PdfViewerScreen.js
+++ client/src/screens/PdfViewerScreen.js
@@ -3,25 +3,25 @@ import { View, TouchableOpacity, Text } from 'react-native';
 import Pdf from 'react-native-pdf';
 import { useNavigation } from '@react-navigation/native';
 
-const PdfViewerScreen = ({route}) => {
+const PdfViewerScreen = ({ route }) => {
   const { pdfUrl, workId, productId } = route.params;
   const navigation = useNavigation();
 
-  const source = {uri: pdfUrl, cache: true};
+  const source = { uri: pdfUrl, cache: true };
 
   return (
-    <View style={{flex: 1}}>
+    <View style={{ flex: 1 }}>
       <Pdf
         trustAllCerts={false}
         source={source}
         onError={(error) => {
           console.log(error);
         }}
-        style={{flex: 1}}
+        style={{ flex: 1 }}
       />
       <View
         style={{
-          position: 'absolute', // Bu satırı ekledim
+          position: 'absolute',
           left: 10,
           bottom: 30,
           backgroundColor: 'turquoise',
@@ -29,11 +29,12 @@ const PdfViewerScreen = ({route}) => {
           borderRadius: 10,
         }}>
         <TouchableOpacity
-          onPress={() => navigation.navigate('WorkOrderScreen', {
-            workId: workId,
-            productId: productId,
-          })}
-        >
+          onPress={() =>
+            navigation.navigate('WorkOrderScreen', {
+              workId: workId,
+              productId: productId,
+            })
+          }>
           <Text style={{ color: 'white', fontSize: 16 }}>İşe Git</Text>
         </TouchableOpacity>
       </View>
