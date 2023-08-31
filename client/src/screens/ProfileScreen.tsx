1b99dab7ff1edc0f9b440d80acb50e591b0b3608
bbalbayrak <b.b.albayrak@hotmail.com>
2023-08-31 09:17:28 +0300

    fixed Cached Profile

--- client/src/screens/ProfileScreen.tsx
+++ client/src/screens/ProfileScreen.tsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { removeData, getData } from '../utils/storage';
-import { getUserInfo } from '../services/profileService';
+import React, {useState, useEffect} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {removeData, getData} from '../utils/storage';
+import {getUserInfo} from '../services/profileService';
 import profilestyles from '../components/Profile';
-import { useIsFocused } from '@react-navigation/native';
+import {useIsFocused} from '@react-navigation/native';
 
 type RootStackParamList = {
   Login: undefined;
@@ -20,7 +20,7 @@ type ProfileScreenNavigationProp = StackNavigationProp<
 const ProfileScreen = () => {
   const navigation = useNavigation<ProfileScreenNavigationProp>();
   const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
-  const isFocused = useIsFocused(); 
+  const isFocused = useIsFocused();
 
   type UserInfo = {
     id: number;
@@ -28,7 +28,7 @@ const ProfileScreen = () => {
     phone: string;
     role: string;
     related_company: string;
-  }
+  };
 
   useEffect(() => {
     const fetchUserInfo = async () => {
@@ -37,19 +37,23 @@ const ProfileScreen = () => {
         const result = await getUserInfo(token);
         setUserInfo(result);
       }
+      const cachedData = await getData('userInfo');
+      const parsedCachedData = JSON.parse(cachedData);
+      return parsedCachedData;
     };
-  
-    if (isFocused) { // if the screen is focused, fetch data
+
+    if (isFocused) {
+      // if the screen is focused, fetch data
       fetchUserInfo();
     }
   }, [isFocused]);
-  
+
   const handleLogout = async () => {
     await removeData('userToken');
     setUserInfo(null); // userInfo state'ini sıfırlıyoruz
     navigation.navigate('Login');
   };
-  
+
   return (
     <View style={profilestyles.container}>
       {userInfo ? (
@@ -80,10 +84,12 @@ const ProfileScreen = () => {
             <Text style={profilestyles.value}>{userInfo?.related_company}</Text>
           </View>
           <View style={profilestyles.buttonContainer}>
-          <TouchableOpacity onPress={handleLogout} style={profilestyles.button}>
-            <Text style={profilestyles.buttonText}>Çıkış Yap</Text>
-          </TouchableOpacity>
-        </View>
+            <TouchableOpacity
+              onPress={handleLogout}
+              style={profilestyles.button}>
+              <Text style={profilestyles.buttonText}>Çıkış Yap</Text>
+            </TouchableOpacity>
+          </View>
         </>
       ) : (
         <Text style={profilestyles.loadingText}>Loading...</Text>
