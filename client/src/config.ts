f696e2de8c835946ea8d2c82c841dfbd9aa7894e
emremataraci <mtrcemre@gmail.com>
2024-07-18 11:47:21 +0300

    test

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,3 +1,3 @@
 export const API_BASE_URL = 'http://10.0.2.2:3005/api';
 export const CLIENT_BASE_URL = 'http://localhost:3000';
-export const ODOO_API_BASE_URL = 'https://portal-steelify-staging-13657712.dev.odoo.com/api';
+export const ODOO_API_BASE_URL = 'https://portal-steelify-staging-13657712.dev.odoo.com/api';d
